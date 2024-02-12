e73d8d1f73eda37209c12770e220bfeaf0da1bbf
Md. Mamun <82976724+MamunKhan71@users.noreply.github.com>
2024-02-12 09:03:10 +0600

    Minor Improvements!

--- age calculator/age.js
+++ age calculator/age.js
@@ -1,30 +1,27 @@
 function calculateAge() {
     const date = document.getElementById('date').value.trim();
     const bornDate = new Date(date);
-    const dateToday = new Date()
-    const birthDate = bornDate.getDate();
-    const birthMonth = bornDate.getMonth() + 1;
-    const birthYear = bornDate.getFullYear();
-
-    const todayDate = dateToday.getDate();
-    const todayMonth = dateToday.getMonth() + 1;
-    const todayYear = dateToday.getFullYear();
-
-    document.getElementById('result').innerHTML = `Your age is : <span class='font-semibold'>${todayYear - birthYear}</span> year, <span class='font-semibold'>${todayMonth - birthMonth}</span> Month and <span class='font-semibold'>${todayDate - birthDate}</span> days`;
+    const dateToday = new Date();
+    console.log(dateToday)
+    const milliseconds = Math.abs(dateToday - bornDate);
+    seconds = milliseconds / 1000;
+    minutes = milliseconds / (1000 * 60);
+    hours = milliseconds / (1000 * 60 * 60);
+    days = milliseconds / (1000 * 60 * 60 * 24);
+    weeks = milliseconds / (1000 * 60 * 60 * 24 * 7);
+    months = milliseconds / (1000 * 60 * 60 * 24 * 30.436875);
+    years = milliseconds / (1000 * 60 * 60 * 24 * 365.25);
+    const remainingMonths = Math.floor(months % 12);
+    const remainingDays = Math.floor(days % 30.436875);
+    document.getElementById('result').innerHTML = `Your age is : <span class='font-semibold'>${Math.round(years)}</span> year, <span class='font-semibold'>${remainingMonths}</span> Month and <span class='font-semibold'>${remainingDays}</span> days`;
     document.getElementById('lastPart').appendChild(document.createElement('hr'));
     document.getElementById('subfield').classList.remove('hidden')
-    const milliseconds = Math.floor((dateToday - bornDate));
-    const seconds = Math.floor(milliseconds / 1000);
-    const minute = Math.floor(seconds / 60);
-    const hour = Math.floor(minute / 60);
-    const day = Math.floor(hour / 24);
-    const week = Math.floor(day / 7);
-    const month = Math.floor(day / 30.436875);
-    document.getElementById('month').innerHTML = month;
-    document.getElementById('weeks').innerHTML = week;
-    document.getElementById('days').innerHTML = day;
-    document.getElementById('minutes').innerHTML = minute;
-    document.getElementById('seconds').innerHTML = seconds;
-    document.getElementById('milliseconds').innerHTML = milliseconds;
+    document.getElementById('month').innerHTML = Math.round(months);
+    document.getElementById('weeks').innerHTML = Math.round(weeks);
+    document.getElementById('days').innerHTML = Math.round(days);
+    document.getElementById('hours').innerHTML = Math.round(hours);
+    document.getElementById('minutes').innerHTML = Math.round(minutes);
+    document.getElementById('seconds').innerHTML = Math.round(seconds);
+    document.getElementById('milliseconds').innerHTML = Math.round(milliseconds);
 
 }
