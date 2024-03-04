70e0514e9f4604d1fc3cb948339f4adcbb7e3656
Md. Mamun <82976724+MamunKhan71@users.noreply.github.com>
2024-03-05 00:21:34 +0600

    Fixed blank input issue!

--- QrCodeGenerator/scripts/scripts.js
+++ QrCodeGenerator/scripts/scripts.js
@@ -1,25 +1,30 @@
 
 function getDetails() {
     let url = document.getElementById("linkUrl");
-    let holder = document.getElementById("placeHolder");
-    let qrCodeText = document.getElementById("qrcode");
-    qrCodeText.innerHTML = "";
-    const newQr = new QRious({
-        element: qrCodeText,
-        value: url.value,
-        size: 600,
-        padding: 50,
-    });
-    url.value = "";
-    holder.innerHTML = "";
-    const newElement = document.createElement('img');
-    const hrTag = document.createElement('hr');
-    hrTag.classList.add('border', 'border-dashed', 'border-gray-400')
-    newElement.classList.add('w-full', 'h-full', 'rounded-t-2xl', 'shadow-lg')
-    newElement.src = newQr.toDataURL();
-    holder.appendChild(newElement);
-    holder.appendChild(hrTag);
-    document.getElementById('downloadBtn').classList.remove('hidden')
+    if (url.value == "") {
+        alert("Please insert any text");
+    } else {
+        let holder = document.getElementById("placeHolder");
+        let qrCodeText = document.getElementById("qrcode");
+        qrCodeText.innerHTML = "";
+        const newQr = new QRious({
+            element: qrCodeText,
+            value: url.value,
+            size: 600,
+            padding: 50,
+        });
+        url.value = "";
+        holder.innerHTML = "";
+        const newElement = document.createElement('img');
+        const hrTag = document.createElement('hr');
+        hrTag.classList.add('border', 'border-dashed', 'border-gray-400');
+        newElement.classList.add('w-full', 'h-full', 'rounded-t-2xl', 'shadow-lg');
+        newElement.src = newQr.toDataURL();
+        holder.appendChild(newElement);
+        holder.appendChild(hrTag);
+        document.getElementById('downloadBtn').classList.remove('hidden');
+    }
+
 }
 function download() {
     let link = document.createElement("a");
