2f31d472a10b0b1cc099636568fdefa8f6618897
Md. Mamun <82976724+MamunKhan71@users.noreply.github.com>
2024-03-06 00:38:03 +0600

    Prevented browser default behaviour on pressing Function keys Like F1 to F12.

--- key codes/script.js
+++ key codes/script.js
@@ -1,24 +1,47 @@
 const keyBox = document.getElementById('keyPressed');
 const keyCode = document.getElementById('keyCode');
+const specialKeys = ["Backspace", "Escape", "Tab", "F1", "F2", "F3", "F4", "F5", "F6", "F7", "F8", "F9", "F10", "F11", "F12", "Space", "Alt", "Control", "CapsLock", "Meta", "ContextMenu", "Shift", "Enter"]
+const duplicateKeys = ["Shift", "Control", "Alt", "Meta"];
+
 window.addEventListener('keydown', (e) => {
+    if (specialKeys.includes(e.key)) {
+        e.preventDefault()
+    };
     if (e.key == " ") {
         keyBox.value = "Space";
         colorChange("Space");
     }
-     else {
+    else {
         keyBox.value = e.key;
         colorChange(e.key);
     }
     keyCode.value = (e.key).charCodeAt(0);
 });
 
+const duplicateValue = (keyIdEvent, dupliId) => {
+    keyIdEvent.classList.add('bg-amber-400');
+    document.getElementById(`${dupliId}2`).classList.add('bg-amber-400')
+    setTimeout(() => {
+        keyIdEvent.classList.remove('bg-amber-400');
+        document.getElementById(`${dupliId}2`).classList.remove('bg-amber-400')
+    }, 200);
+}
 
 const colorChange = (keyId) => {
-    const keyIdEvent = document.getElementById(String(keyId).toLowerCase());
-    keyIdEvent.classList.add('bg-amber-400');
-    setTimeout(()=>{
-        keyIdEvent.classList.remove('bg-amber-400');
-    },200)
+    let keyIdEvent;
+    if (specialKeys.includes(keyId)) {
+        keyIdEvent = document.getElementById(String(keyId));
+    } else {
+        keyIdEvent = document.getElementById(String(keyId).toLowerCase());
+    }
+    if (duplicateKeys.includes(keyId)) {
+        duplicateValue(keyIdEvent, keyId);
+    } else {
+        keyIdEvent.classList.add('bg-amber-400');
+        setTimeout(() => {
+            keyIdEvent.classList.remove('bg-amber-400');
+        }, 200);
+    }
 }
 
 const copyCode = () => {
