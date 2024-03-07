f2e640b55b7b0b34c99664b67307d6fb63859e28
Md. Mamun <82976724+MamunKhan71@users.noreply.github.com>
2024-03-07 17:08:12 +0600

    Css improvements!

--- drag and drop/script.js
+++ drag and drop/script.js
@@ -2,25 +2,28 @@ const rightBox = document.getElementById('rightBox');
 const leftBox = document.getElementById('leftBox');
 const boxes = document.getElementsByClassName('lists');
 
-for(const box of boxes){
+for (const box of boxes) {
     box.addEventListener('dragstart', function (e) {
         let selected = e.target;
         rightBox.addEventListener('dragover', function (e) {
             e.preventDefault();
+            rightBox.classList.add('bg-base-200', 'opacity-20');
         });
 
-        rightBox.addEventListener('drop', function(e){
+        rightBox.addEventListener('drop', function (e) {
             rightBox.appendChild(selected);
             box.classList.add('animate-pulse', 'animate-once');
+            rightBox.classList.remove('bg-base-200', 'opacity-20');
             selected = null;
         });
         leftBox.addEventListener('dragover', function (e) {
             e.preventDefault();
+            rightBox.classList.remove('bg-base-200', 'opacity-20');
+
         });
 
-        leftBox.addEventListener('drop', function(e){
-            box.classList.remove('animate-pulse', 'animate-once');
-
+        leftBox.addEventListener('drop', function (e) {
+            rightBox.classList.remove('bg-base-200', 'opacity-20');
             leftBox.appendChild(selected);
             selected = null;
         });
